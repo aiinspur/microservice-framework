f586cf3e1cea99ca44161c2dfb6e85a06a1f50a9
aiinspur <aiinspur@outlook.com>
2019-11-05 17:40:45 +0800

    add mgn module

---
+++ msf-mng/src/main/java/com/msf/msfmng/MsfMngApplication.java
@@ -0,0 +1,18 @@
+package com.msf.msfmng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author shihujiang
+ * @date 2019-11-05
+ */
+@SpringBootApplication
+public class MsfMngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsfMngApplication.class, args);
+    }
+
+}
---
+++ msf-mng/src/test/java/com/msf/msfmng/MsfMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msf.msfmng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsfMngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
